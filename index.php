667229e8a3d6c3f77d69cc9d82d0a4ac9e3c8372
cloudadmin1 <98026405+cloudadmin1@users.noreply.github.com>
2022-02-05 07:55:39 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
 
-echo "Hello World! my Name is Md Murtuza ";
+echo "Hello World! my Name is Md Murtuza Azure Administrator/Office 365 Administrator, 02/2020 –12/2021
+ Wipro Technology Ltd.                                                                         Delhi
+Environment: Azure, Azure AD Connect, Active Sync and Office 365.
+
+⦁	Migration, Azure Load balancing, Networks, Backups, App Services, Security Center IaaS and PaaS
+⦁	Migrated the on-premises workloads to Office 365 / Azure cloud based on the requirement.
+⦁	Design and deploy Azure Backup and other Confidential backup solutions for Azure
+⦁	Strong design and live migration experience from on-premises to Azure IaaS & PaaS
+⦁	Azure network experience, VPN and Express route, Azure DNS, Traffic Manager, and Load Balancers.
+⦁	Responsible for systems and application performance monitoring.
+⦁	Strong Proficiency in all the services offered by Confidential Azure including IaaS, PaaS and SaaS.
+⦁	Implementation experience on AD B2C Custom Attributes, Role based access (RBAC) and AD Groups.
+⦁	To ensure the availability, performance, scalability and security of production systems.
+⦁	Management and support of DFS, disk quotas, home folders, and terminal services.
+⦁	Password policies and Account lockout policies
+⦁	Build of new environments for development and test in Azure.
+⦁	Responsible for managing network storage in Azure
+⦁	Troubleshoot and customize Office 365 applications including Word, Excel, OneNote, skype for business, Outlook, Yammer, Power Point, and Microsoft  ";
